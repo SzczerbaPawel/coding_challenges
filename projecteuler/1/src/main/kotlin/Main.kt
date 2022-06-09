23e81886ef85edfc3b1be9c64fd793f16c60f07b
Pawel Szczerba <szczerba.paw@gmail.com>
2022-06-09 21:14:27 +0200

    1@projecteuler

---
+++ projecteuler/1/src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+    val start = System.currentTimeMillis()
+    var sum=0
+
+//     1324 ms for range 1-1000000000:
+//    for (i in 3..1000) {
+//        if(i%3==0 || i%5==0)
+//            sum+=i
+//    }
+
+//    312 ms for range 1-1000000000
+    for (i in 3..1000 step 3) {
+        sum+=i
+    }
+    for (i in 5..1000 step 5) {
+        sum+=i
+    }
+    for (i in 15..1000 step 15) {
+        sum-=i
+    }
+
+    val end = System.currentTimeMillis()
+    println("Answer: $sum")
+    println("Time: ${end - start} ms")
+}
