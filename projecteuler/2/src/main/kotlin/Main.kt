569689d267658aa5647e0ecf3fece919f5dfe954
Pawel Szczerba <szczerba.paw@gmail.com>
2022-06-10 20:21:20 +0200

    2@projecteuler

---
+++ projecteuler/2/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var first = 1
+    var second = 2
+    var third = 0
+    // including 2 in the answer already
+    var answer = 2
+    do {
+        third=first+second
+        if (third % 2 == 0 )
+            answer += third
+        first = second
+        second = third
+    } while (third < 4000000)
+    println("Answer: $answer")
+}
